fb5ab5bc8c7a36ca33e63371c309286d53fa04f5
AICHEN <764668210@qq.com>
2018-11-01 18:39:07 +0800

    Add Message

---
+++ Pokemon Battle Simulator/Assets/Scripts/Data/Messages/BattleStartMessage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BattleStartMessage : MessageBase
+{
+    public static readonly short type = 100;
+    public int pokemonId;
+    public int hpAv;
+    public int attackAv;
+    public int defenceAv;
+    public int sp_attackAv;
+    public int sp_defenceAv;
+    public int speedAv;
+    public int charavterId;
+    public int personalityId;
+    public int itemId;
+    public int[] skillIds = new int[4];
+    public BattleStartMessage(Pokemon _pokemon)
+    {
+        PokemonModel pM = _pokemon.GetModel();
+        CharacterModel cM = _pokemon.GetCharacter().GetModel();
+        PersonalityModel perM = _pokemon.GetPersonality().GetModel();
+        ItemModel iM = _pokemon.GetItem().GetModel();
+        Skill[] skM = _pokemon.GetSkills();//固定为4个技能
+        pokemonId = pM.id;
+        hpAv = pM.hp;
+        attackAv = pM.attack;
+        defenceAv = pM.defense;
+        sp_attackAv = pM.sp_attack;
+        sp_defenceAv = pM.sp_defense;
+        speedAv = pM.speed;
+        charavterId = cM.id;
+        personalityId = perM.id;
+        itemId = iM.id;
+        for(int i=0;i<4;i++)
+        {
+            skillIds[i] = skM[i].GetModel().id;
+        }
+    }
+}
---
+++ Pokemon Battle Simulator/Assets/Scripts/Data/Messages/ChangePokemonMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ChangePokemonMessage : MessageBase
+{
+    public static readonly short type=101;
+    public int index;
+    public int pokemonId;
+    public ChangePokemonMessage(int _index,int _id)
+    {
+        index = _index;
+        pokemonId = _id;
+    }
+}
